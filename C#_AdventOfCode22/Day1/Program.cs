bad39eec548117b1bbb8efd50a0bd307dfccc812
Benjamin <benjamin.vd.wolf@movares.nl>
2022-12-26 09:15:53 +0100

    add C# advent of code solution

---
+++ C#_AdventOfCode22/Day1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day1
+{
+    public class Testable
+    {
+        public int Multiply(int lhs, int rhs)
+        {
+            return (lhs * rhs);
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
